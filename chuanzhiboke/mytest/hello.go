cce1b6d16914887fb4b5b768adb33214a936fc32
yaoyaoz <905440208@qq.com>
2019-07-27 12:47:06 +0800

    go语言基础

--- chuanzhiboke/mytest/hello.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"	//我们需要fmt包中的Print函数
-
-func main() {
-	fmt.Println("hello world");
-}
---
+++ chuanzhiboke_20day_rumen/06_多重赋值和匿名变量.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+//go函数可以返回多个值
+func test() (a, b, c int) {
+	return 1, 2, 3
+}
+
+func main() {
+	//a := 10
+	//b := 20
+	//c := 30
+
+	a, b := 10, 20
+
+	//交换两个变量的值(传统的方法：定义个中间变量tmp)
+	var tmp int
+	tmp = a
+	a = b
+	b = tmp
+	fmt.Printf("a = %d, b = %d\n", a, b)
+
+	//go语言的特性：直接交换两个变量的值
+	i, j := 10, 20
+	i, j = j, i
+	fmt.Printf("i = %d, j = %d\n", i, j)
+
+	//_匿名变量，丢弃数据不处理，_匿名变量配合函数返回值使用，才有优势
+	tmp, _ = i, j
+	fmt.Println("tmp = ", tmp)
+
+	var c, d, e int
+	c, d, e = test() //return 1, 2, 3
+	fmt.Printf("c = %d, d = %d, e = %d\n", c, d, e)
+
+	_, d, _ = test() //return 1, 2, 3
+	fmt.Println("d = ", d)
+}
